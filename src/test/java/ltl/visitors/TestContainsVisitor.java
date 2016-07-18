709bb3f3b06630d382e7ddbabddcaa4a8fc9c162
ga25suc <ga25suc@mytum.de>
2016-07-18 14:39:09 +0200

    [ltl-lib] fixed bug in containsVisitor and added a test revealing it

---
+++ src/test/java/ltl/visitors/TestContainsVisitor.java
@@ -0,0 +1,22 @@
+package ltl.visitors;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import ltl.Formula;
+import ltl.Literal;
+import ltl.ROperator;
+import ltl.UOperator;
+
+public class TestContainsVisitor {
+
+    /**
+     * This test checks, whether there is an AssertionError thrown.
+     */
+    @Test
+    public void testContainsVisitorROperator() {
+        Formula f = new ROperator(new Literal(0), new Literal(1));
+        assertFalse(f.accept(new ContainsVisitor(UOperator.class)));
+    }
+}
