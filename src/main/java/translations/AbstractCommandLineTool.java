8c65020b9ce03dafcd72b0c00da0cecb7de026b9
Salomon Sickert <sickert@in.tum.de>
2017-01-04 16:27:28 +0100

    Test constructions also without optimisations.

--- src/main/java/translations/AbstractCommandLineTool.java
+++ src/main/java/translations/AbstractCommandLineTool.java
@@ -25,21 +25,65 @@ import omega_automaton.output.HOAPrintable;
 import java.io.StringReader;
 import java.util.Deque;
 import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 import java.util.function.Function;
 
 public abstract class AbstractCommandLineTool {
 
+    private static final String ANNOTATIONS = "--annotations";
+    private static final String OPTIMISATIONS = "--optimisations=";
+
     protected abstract Function<Formula, ? extends HOAPrintable> getTranslation(EnumSet<Optimisation> optimisations);
 
+    private EnumSet<HOAPrintable.Option> parseHOAOutputOptions(Deque<String> args) {
+        return args.remove(ANNOTATIONS)
+                ? EnumSet.of(HOAPrintable.Option.ANNOTATIONS)
+                : EnumSet.noneOf(HOAPrintable.Option.class);
+    }
+
+    private EnumSet<Optimisation> parseOptimisationOptions(Deque<String> args) {
+        for (Iterator<String> iterator = args.iterator(); iterator.hasNext(); ) {
+            String argument = iterator.next();
+
+            if (argument.startsWith(OPTIMISATIONS)) {
+                iterator.remove();
+                argument = argument.substring(OPTIMISATIONS.length());
+
+                switch (argument) {
+                    case "all":
+                        return EnumSet.allOf(Optimisation.class);
+
+                    case "none":
+                        return EnumSet.noneOf(Optimisation.class);
+
+                    default:
+                        StringTokenizer tokenizer = new StringTokenizer(argument, "([{,;])");
+                        EnumSet<Optimisation> set = EnumSet.noneOf(Optimisation.class);
+
+                        while (tokenizer.hasMoreTokens()) {
+                            set.add(Optimisation.valueOf(tokenizer.nextToken()));
+                        }
+
+                        return set;
+                }
+            }
+        }
+
+        return EnumSet.allOf(Optimisation.class);
+    }
+
     void execute(Deque<String> args) throws Exception {
-        // TODO: Parse Optimisations
-        Function<Formula, ? extends HOAPrintable> translation = getTranslation(EnumSet.allOf(Optimisation.class));
+        EnumSet<HOAPrintable.Option> options = parseHOAOutputOptions(args);
+        EnumSet<Optimisation> optimisations = parseOptimisationOptions(args);
+
+        Function<Formula, ? extends HOAPrintable> translation = getTranslation(optimisations);
 
         Parser parser = new Parser(new StringReader(args.removeLast()));
         Formula formula = parser.formula();
 
         HOAPrintable result = translation.apply(formula);
         result.setAtomMapping(parser.map.inverse());
-        result.toHOA(new HOAConsumerPrint(System.out));
+        result.toHOA(new HOAConsumerPrint(System.out), options);
     }
 }
