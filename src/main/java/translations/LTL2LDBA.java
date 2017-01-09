760c926cf66bf34dd9b0d2d065025ce0c3a8f4d4
Salomon Sickert <sickert@in.tum.de>
2017-01-09 14:53:56 +0100

    Consolidate different translation into a single tool.

--- src/main/java/translations/LTL2LDBA.java
+++ src/main/java/translations/LTL2LDBA.java
@@ -20,19 +20,61 @@ package translations;
 import ltl.Formula;
 import omega_automaton.output.HOAPrintable;
 import translations.ltl2ldba.Ltl2Ldba;
+import translations.ltl2ldba.Ltl2Ldgba;
+import translations.ltl2ldba.ng.Ltl2LdbaNg;
+import translations.ltl2ldba.ng.Ltl2LdgbaNg;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.EnumSet;
 import java.util.function.Function;
 
 public class LTL2LDBA extends AbstractCommandLineTool {
-    @Override
-    protected Function<Formula, ? extends HOAPrintable> getTranslation(EnumSet<Optimisation> optimisations) {
-        return new Ltl2Ldba(optimisations);
+    enum Configuration {
+        GENERALISED, GUESS_F, // NONDET_INITIAL_COMPONENT
     }
 
-    public static void main(String... args) throws Exception {
-        new LTL2LDBA().execute(new ArrayDeque<>(Arrays.asList(args)));
+    private final Function<EnumSet<Optimisation>, Function<Formula, ? extends HOAPrintable>> constructor;
+
+    private LTL2LDBA(EnumSet<Configuration> configuration) {
+        if (configuration.contains(Configuration.GENERALISED)) {
+            if (configuration.contains(Configuration.GUESS_F)) {
+                constructor = Ltl2LdgbaNg::new;
+            } else {
+                constructor = Ltl2Ldgba::new;
+            }
+        } else {
+            if (configuration.contains(Configuration.GUESS_F)) {
+                constructor = Ltl2LdbaNg::new;
+            } else {
+                constructor = Ltl2Ldba::new;
+            }
+        }
+    }
+
+    @Override
+    protected Function<Formula, ? extends HOAPrintable> getTranslation(EnumSet<Optimisation> optimisations) {
+        return constructor.apply(optimisations);
+    }
+
+    public static void main(String... argsArray) throws Exception {
+        Deque<String> args = new ArrayDeque<>(Arrays.asList(argsArray));
+
+        EnumSet<Configuration> configuration = EnumSet.of(Configuration.GENERALISED);
+
+        if (args.remove("--Büchi") || args.remove("--Buchi")) {
+            configuration.remove(Configuration.GENERALISED);
+        }
+
+        if ((args.remove("--generalised-Büchi") || args.remove("--generalised-Buchi")) && !configuration.contains(Configuration.GENERALISED)) {
+            throw new RuntimeException("Invalid arguments: " + Arrays.toString(argsArray));
+        }
+
+        if (args.remove("--guess-F")) {
+            configuration.add(Configuration.GUESS_F);
+        }
+
+        new LTL2LDBA(configuration).execute(args);
     }
 }
