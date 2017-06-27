9c464e47703dd8eb85bf91a72860580553a2b939
Salomon Sickert <sickert@in.tum.de>
2017-06-27 10:15:36 +0200

    Add missing header.

--- src/main/java/owl/util/annotation/package-info.java
+++ src/main/java/owl/util/annotation/package-info.java
@@ -15,18 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+@EverythingIsNonnullByDefault
 package owl.util.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import javax.annotation.Nonnull;
-import javax.annotation.meta.TypeQualifierDefault;
-
-@Documented
-@Nonnull
-@TypeQualifierDefault(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ReturnValueIsNonnullByDefault {
-}
