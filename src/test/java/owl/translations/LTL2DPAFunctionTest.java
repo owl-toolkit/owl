c93f72df2bf2616c3f1c71d40244b0862c3e2e0f
Salomon Sickert <sickert@in.tum.de>
2018-12-10 10:09:56 +0100

    Ensure a minimal number of acceptance sets, such that completing an automaton always succeeds.

---
+++ src/test/java/owl/translations/LTL2DPAFunctionTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 - 2018  (See AUTHORS)
+ *
+ * This file is part of Owl.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package owl.translations;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import owl.ltl.parser.LtlParser;
+import owl.run.DefaultEnvironment;
+import owl.translations.ltl2dpa.LTL2DPAFunction;
+
+public class LTL2DPAFunctionTest {
+
+  @Test
+  void testComplete() {
+    var environment = DefaultEnvironment.standard();
+    var translation = new LTL2DPAFunction(environment, Set.of(
+      LTL2DPAFunction.Configuration.OPTIMISE_INITIAL_STATE,
+      LTL2DPAFunction.Configuration.COMPLETE,
+      LTL2DPAFunction.Configuration.EXISTS_SAFETY_CORE,
+      LTL2DPAFunction.Configuration.COMPRESS_COLOURS));
+
+    translation.apply(LtlParser.parse("ff"));
+  }
+}
