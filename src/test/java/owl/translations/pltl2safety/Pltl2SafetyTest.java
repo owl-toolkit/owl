4335ebb33b571d2a962bf320b0db97e2390496ac
Florian Stamer <ga65zuy@mytum.de>
2019-01-11 15:23:18 +0100

    Changes to pltl2safety construction:
     - Renamed Pltl2SafetyFunction to Pltl2Safety
     - Created a test file for pltl2safety construction

--- src/test/java/owl/translations/pltl2safety/Pltl2SafetyTest.java
+++ src/test/java/owl/translations/pltl2safety/Pltl2SafetyTest.java
@@ -1,4 +1,31 @@
 package owl.translations.pltl2safety;
 
-public class Pltl2SafetyTest {
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import owl.ltl.LabelledFormula;
+import owl.ltl.parser.SpectraParser;
+import owl.run.DefaultEnvironment;
+
+class Pltl2SafetyTest {
+
+  private static final List<String> INPUT = List.of(
+    "sys boolean a;"
+    + "gar G H a;",
+    "env boolean a;"
+    + "env boolean b;"
+    + "asm G a S b;",
+    "sys boolean a;"
+    + "sys boolean b;"
+    + "gar G a S (H b);"
+  );
+
+  @Test
+  void test() {
+    PLTL2Safety safetyConstructor = new PLTL2Safety(DefaultEnvironment.standard());
+    for (String in : INPUT) {
+      List<LabelledFormula> safety = SpectraParser.parse(in).getLabelledSafety();
+      safety.forEach(safetyConstructor::apply);
+    }
+  }
 }
