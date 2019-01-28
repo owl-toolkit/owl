0f0cb6524995994183a55e92acec582bdd3f986c
Florian Stamer <ga65zuy@mytum.de>
2019-01-28 14:22:49 +0100

    fix package structure

--- src/test/java/owl/translations/pltl2safety/Pltl2SafetyTest.java
+++ src/test/java/owl/translations/pltl2safety/Pltl2SafetyTest.java
@@ -1,10 +1,11 @@
-package owl.translations.pltl2safety;
+package owl.translations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static owl.util.Assertions.assertThat;
 
+import java.util.EnumSet;
 import java.util.List;
 
 import jhoafparser.parser.generated.ParseException;
@@ -21,11 +22,14 @@ import owl.automaton.acceptance.BuchiAcceptance;
 import owl.automaton.algorithms.LanguageAnalysis;
 import owl.automaton.output.HoaPrinter;
 import owl.ltl.LabelledFormula;
+import owl.ltl.parser.LtlParser;
 import owl.ltl.parser.SpectraParser;
 import owl.run.DefaultEnvironment;
+import owl.run.Environment;
+import owl.translations.LTL2DAFunction;
 
 class Pltl2SafetyTest {
-  private final PLTL2Safety translator = new PLTL2Safety(DefaultEnvironment.standard());
+  private static final Environment environment = DefaultEnvironment.standard();
 
   private static final List<String> INPUT = List.of(
     "sys boolean a;\n"
@@ -41,8 +45,8 @@ class Pltl2SafetyTest {
   @Test
   void test() {
     for (String in : INPUT) {
-      List<LabelledFormula> safety = SpectraParser.parse(in).getLabelledSafety();
-      safety.forEach(translator::apply);
+      List<LabelledFormula> safety = SpectraParser.parse(in).getSafety();
+      safety.forEach(x -> LTL2DAFunction.pSafety(environment, x));
     }
   }
 
@@ -54,19 +58,14 @@ class Pltl2SafetyTest {
         Arguments.of(
           "env boolean a;\n"
             + "asm G Y a;",
-          2,
+          1,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 1 \"a\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[p0], []]\"\n"
-            + "[!0] 0\n"
-            + "[0] 1\n"
-            + "State: 1 \"[[Yp0], [p0, Yp0], [], [p0]]\"\n"
-            + "[!0] 0\n"
-            + "[0] 1\n"
+            + "State: 0 \"[]\"\n"
             + "--END--\n"
         ),
         Arguments.of(
@@ -79,10 +78,10 @@ class Pltl2SafetyTest {
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Zp0, p0], [Zp0]]\"\n"
+            + "State: 0 \"[[p0, Zp0], [Zp0, !p0]]\"\n"
             + "[0] 0\n"
             + "[!0] 1\n"
-            + "State: 1 \"[[p0], []]\"\n"
+            + "State: 1 \"[[p0, Y!p0], [!p0, Y!p0]]\"\n"
             + "[0] 0\n"
             + "[!0] 1\n"
             + "--END--\n"
@@ -90,18 +89,20 @@ class Pltl2SafetyTest {
         Arguments.of(
           "env boolean a;\n"
             + "asm G H a;",
-          2,
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 1 \"a\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Hp0, p0], []]\"\n"
-            + "[0] 0\n"
-            + "[!0] 1\n"
-            + "State: 1 \"[[p0],[]]\"\n"
-            + "[t] 1\n"
+            + "State: 0 \"[[Hp0, p0]]\"\n"
+            + "[0] 1\n"
+            + "State: 1 \"[[!p0, O!p0], [p0, Hp0]]\"\n"
+            + "[0] 1\n"
+            + "[!0] 2\n"
+            + "State: 2 \"[[!p0, O!p0], [p0, O!p0]]\"\n"
+            + "[t] 2\n"
             + "--END--\n"
         ),
         Arguments.of(
@@ -114,69 +115,78 @@ class Pltl2SafetyTest {
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Op0, p0], []]\"\n"
+            + "State: 0 \"[[p0, Op0]]\"\n"
             + "[0] 1\n"
-            + "[!0] 0\n"
-            + "State: 1 \"[[Op0, p0],[Op0]]\"\n"
+            + "State: 1 \"[[p0, Op0], [!p0, Op0]]\"\n"
             + "[t] 1\n"
             + "--END--\n"
         ),
         Arguments.of(
           "env boolean a;\n"
-          + "env boolean b;\n"
-          + "asm G H a;",
-          2,
+            + "env boolean b;\n"
+            + "asm G H a;",
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
-            + "AP: 2 \"a\" \"b\"\n"
+            + "AP: 1 \"a\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Hp0, p0], []]\"\n"
-            + "[0] 0\n"
-            + "[!0] 1\n"
-            + "State: 1 \"[[p0],[]]\"\n"
-            + "[t] 1\n"
+            + "State: 0 \"[[Hp0, p0]]\"\n"
+            + "[0] 1\n"
+            + "State: 1 \"[[!p0, O!p0], [p0, Hp0]]\"\n"
+            + "[0] 1\n"
+            + "[!0] 2\n"
+            + "State: 2 \"[[!p0, O!p0], [p0, O!p0]]\"\n"
+            + "[t] 2\n"
             + "--END--\n"
         ),
         Arguments.of(
           "sys boolean a;\n"
             + "sys boolean b;\n"
             + "gar G a S b;",
-          2,
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 2 \"a\" \"b\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[(p0Sp1), p0, p1], [(p0Sp1), p1], [p0], []]\"\n"
-            + "[!1] 0\n"
+            + "State: 0 \"[[!p0, p1, (p0Sp1)], [p0, p1, (p0Sp1)]]\"\n"
             + "[1] 1\n"
-            + "State: 1 \"[[(p0Sp1), p0, p1], [(p0Sp1), p1], [(p0Sp1), p0], []]\"\n"
+            + "State: 1 \"[[!p0, !p1, (!p0T!p1)], [p0, !p1, (p0Sp1)]," +
+            "[!p0, p1, (p0Sp1)], [p0, p1, (p0Sp1)]]\"\n"
             + "[1] 1\n"
-            + "[0] 1\n"
-            + "[!0 & !1] 0\n"
+            + "[0 & !1] 1\n"
+            + "[!0 & !1] 2\n"
+            + "State: 2 \"[[!p0, !p1, (!p0T!p1)], [p0, !p1, (!p0T!p1)]," +
+            "[!p0, p1, (p0Sp1)], [p0, p1, (p0Sp1)]]\"\n"
+            + "[1] 1\n"
+            + "[!1] 2\n"
             + "--END--\n"
         ),
         Arguments.of(
           "sys boolean a;\n"
             + "sys boolean b;\n"
             + "gar G a T b;",
-          2,
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 2 \"a\" \"b\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[(p0Tp1), p0, p1], [(p0Tp1), p1], [p0], []]\"\n"
-            + "[!1] 0\n"
-            + "[1] 1\n"
-            + "State: 1 \"[[(p0Tp1), p0, p1], [p1], [p0], []]\"\n"
-            + "[!1] 0\n"
-            + "[!0 & 1] 0\n"
-            + "[0 & 1] 1\n"
+            + "State: 0 \"[[!p0, p1, (p0Tp1)], [p0, p1, (p0Tp1)]]\"\n"
+            + "[1] 2\n"
+            + "State: 1 \"[[!p0, (!p0S!p1), !p1], [(!p0S!p1), !p1, p0]," +
+            "[!p0, (!p0S!p1), p1], [p0, p1, (p0Tp1)]]\"\n"
+            + "[0 & 1] 2\n"
+            + "[!0 & 1] 1\n"
+            + "[!1] 1\n"
+            + "State: 2 \"[[!p0, (!p0S!p1), !p1], [(!p0S!p1)," +
+            "!p1, p0], [!p0, p1, (p0Tp1)], [p0, p1, (p0Tp1)]]\"\n"
+            + "[1] 2\n"
+            + "[!1] 1\n"
             + "--END--\n"
         ),
         Arguments.of(
@@ -190,75 +200,87 @@ class Pltl2SafetyTest {
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Hp0, p0, p1], [p1]]\"\n"
-            + "[0 & 1] 2\n"
-            + "[!0 & 1] 1\n"
-            + "State: 1 \"[[p0, p1], [p0], [p1], []]\"\n"
-            + "[t] 1\n"
-            + "State: 2 \"[[Hp0, p0, p1], [Hp0, p0], [p1], []]\"\n"
-            + "[0] 2\n"
-            + "[!0] 1\n"
+            + "State: 0 \"[[p0, p1, (Hp0&p1), Hp0]]\"\n"
+            + "[0 & 1] 1\n"
+            + "State: 1 \"[[!p0, !p1, O!p0, (O!p0|!p1)], [p0, p1, Hp0, (Hp0&p1)]," +
+            "[!p0, p1, O!p0, (O!p0|!p1)], [!p1, p0, Hp0, (O!p0|!p1)]]\"\n"
+            + "[0] 1\n"
+            + "[!0] 2\n"
+            + "State: 2 \"[[[!p0, !p1, O!p0, (O!p0|!p1)], [!p1, p0, O!p0, (O!p0|!p1)]," +
+            "[!p0, p1, O!p0, (O!p0|!p1)], [p0, p1, O!p0, (O!p0|!p1)]]\"\n"
+            + "[t] 2\n"
             + "--END--\n"
         ),
         Arguments.of(
           "env boolean a;\n"
             + "env boolean b;\n"
             + "asm G (H a) | b;",
-          2,
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 2 \"a\" \"b\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Hp0, p0, p1], [Hp0, p0], [p1], []]\"\n"
-            + "[0] 0\n"
-            + "[!0] 1\n"
-            + "State: 1 \"[[p0, p1], [p0], [p1], []]\"\n"
-            + "[t] 1\n"
+            + "State: 0 \"[[p0, p1, Hp0, (Hp0|p1)], [!p0, p1, (Hp0|p1), O!p0]," +
+            "[!p1, p0, (Hp0|p1), Hp0]]\"\n"
+            + "[0] 1\n"
+            + "[!0 & 1] 2\n"
+            + "State: 1 \"[[!p0, !p1, O!p0, (O!p0&!p1)], [p0, p1, Hp0, (Hp0|p1)]," +
+            "[!p0, p1, (Hp0|p1), O!p0], [!p1, p0, (Hp0|p1), Hp0]]\"\n"
+            + "[0] 1\n"
+            + "[!0] 2\n"
+            + "State: 2 \"[[!p0, !p1, O!p0, (O!p0&!p1)], [!p1, p0, O!p0, (O!p0&!p1)]," +
+            "[!p0, p1, (Hp0|p1), O!p0], [p0, p1, (Hp0|p1), O!p0]]\"\n"
+            + "[t] 2\n"
             + "--END--\n"
         ),
         Arguments.of(
           "sys boolean a;\n"
-          + "gar G a <-> H a;",
-          3,
+            + "gar G a <-> H a;",
+          2,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 1 \"a\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Hp0, p0]]\"\n"
-            + "[0] 1\n"
-            + "State: 1 \"[[Hp0, p0],[]]\"\n"
-            + "[0] 1\n"
-            + "[!0] 2\n"
-            + "State: 2 \"[[p0], []]\"\n"
-            + "[t] 2\n"
+            + "State: 0 \"[[!p0, ((Hp0&p0)|(O!p0&!p0)), (O!p0|!p0), O!p0, (O!p0&!p0)]," +
+            "[((Hp0&p0)|(O!p0&!p0)), p0, Hp0, (Hp0&p0), (Hp0|p0)]]\"\n"
+            + "[0] 0\n"
+            + "[!0] 1\n"
+            + "State: 1 \"[[!p0, ((Hp0&p0)|(O!p0&!p0)), (O!p0|!p0), O!p0, (O!p0&!p0)]," +
+            "[((Hp0|p0)&(O!p0|!p0)), p0, (O!p0|!p0), O!p0, (Hp0|p0)]]\"\n"
+            + "[t] 1\n"
             + "--END--\n"
         ),
         Arguments.of(
           "sys boolean a;\n"
-          + "sys boolean b;\n"
-          + "gar G a -> Y b;",
-          2,
+            + "sys boolean b;\n"
+            + "gar G a -> Y b;",
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 2 \"a\" \"b\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[p0, p1], [p0], [p1], []]\"\n"
-            + "[!1] 0\n"
-            + "[1] 1\n"
-            + "State: 1 \"[[Yp1, p0, p1], [Yp1, p1], [Yp1, p0], [Yp1]]\"\n"
-            + "[1] 1\n"
-            + "[!1] 0\n"
+            + "State: 0 \"[[!p0, !p1, Z!p1, (Yp1|!p0)], [!p0, p1, Z!p1, (Yp1|!p0)]]\"\n"
+            + "[!0 & !1] 1\n"
+            + "[!0 & 1] 2\n"
+            + "State: 1 \"[[!p0, !p1, Z!p1, (Yp1|!p0)], [!p0, p1, Z!p1, (Yp1|!p0)]," +
+            "[!p1, p0, (Z!p1&p0), Z!p1], [p0, p1, (Z!p1&p0), Z!p1]]\"\n"
+            + "[1] 2\n"
+            + "[!1] 1\n"
+            + "State: 2 \"[[!p1, p0, Yp1, (Yp1|!p0)], [!p0, p1, Yp1, (Yp1|!p0)]," +
+            "[p0, p1, Yp1, (Yp1|!p0)], [!p0, !p1, Yp1, (Yp1|!p0)]]\"\n"
+            + "[1] 2\n"
+            + "[!1] 1\n"
             + "--END--\n"
         ),
         Arguments.of(
           "sys Int(0..2) speed;\n"
-          + "gar G (PREV(speed=0) -> speed=1);",
+            + "gar G (PREV(speed=0) -> speed=1);",
           2,
           "HOA: v1\n"
             + "Start: 0\n"
@@ -266,12 +288,17 @@ class Pltl2SafetyTest {
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[Z(speed0|speed1), speed0, speed1], [Z(speed0|speed1)],"
-            + "[Z(speed0|speed1), speed0], [Z(speed0|speed1)]\"\n"
+            + "State: 0 \"[[!p1, p0, (Z(p1|p0)|(p0&!p1)), (p0&!p1), Z(p1|p0), (p1|p0)]," +
+            "[!p0, !p1, (Z(p1|p0)|(p0&!p1)), (!p1&!p0), Z(p1|p0), (p1|!p0)]," +
+            "[!p0, p1, (Z(p1|p0)|(p0&!p1)), Z(p1|p0), (p1|p0), (p1|!p0)]," +
+            "[p0, p1, (Z(p1|p0)|(p0&!p1)), Z(p1|p0), (p1|p0), (p1|!p0)]]\"\n"
             + "[0] 0\n"
             + "[1] 0\n"
             + "[!0 & !1] 1\n"
-            + "State: 1 \"[[speed0, speed1], [speed0], [speed1], []]\"\n"
+            + "State: 1 \"[[!p0, Y(!p1&!p0), !p1, (!p1&!p0), ((p1|!p0)&Y(!p1&!p0)), (p1|!p0)]," +
+            "[!p0, Y(!p1&!p0), p1, (p1|p0), ((p1|!p0)&Y(!p1&!p0)), (p1|!p0)]," +
+            "[Y(!p1&!p0), p0, p1, (p1|p0), ((p1|!p0)&Y(!p1&!p0)), (p1|!p0)]," +
+            "[Y(!p1&!p0), !p1, p0, (Z(p1|p0)|(p0&!p1)), (p0&!p1), (p1|p0)]]\"\n"
             + "[0] 0\n"
             + "[1] 0\n"
             + "[!0 & !1] 1\n"
@@ -279,22 +306,32 @@ class Pltl2SafetyTest {
         ),
         Arguments.of(
           "env boolean grant;\n"
-          + "sys boolean request;\n"
-          + "gar G (grant -> (!grant S request));",
-          2,
+            + "sys boolean request;\n"
+            + "gar G (grant -> Y (!grant S request));",
+          3,
           "HOA: v1\n"
             + "Start: 0\n"
             + "AP: 2 \"grant\" \"request\"\n"
             + "acc-name: all\n"
             + "Acceptance: 0 t\n"
             + "--BODY--\n"
-            + "State: 0 \"[[(!p0Sp1, p0, p1], [(!p0Sp1), p1], [p0], []]\"\n"
-            + "[!1] 0\n"
-            + "[1] 1\n"
-            + "State: 1 \"[[(!p0Sp1, p0, p1], [(!p0Sp1), p1], [(!p0Sp1)], [p0]]\"\n"
-            + "[0 & !1] 0\n"
+            + "State: 0 \"[[!p0, p1, (Y(!p0Sp1)|!p0), (!p0Sp1), Z(p0T!p1)]," +
+            "[!p0, !p1, (p0T!p1), (Y(!p0Sp1)|!p0), Z(p0T!p1)]]\"\n"
             + "[!0 & !1] 1\n"
-            + "[1] 1\n"
+            + "[!0 & 1] 2\n"
+            + "State: 1 \"[[p0, p1, (!p0Sp1), (Z(p0T!p1)&p0), Z(p0T!p1)]," +
+            "[!p0, p1, (Y(!p0Sp1)|!p0), (!p0Sp1), Z(p0T!p1)]," +
+            "[!p1, p0, (p0T!p1), (Z(p0T!p1)&p0), Z(p0T!p1)]," +
+            "[!p0, !p1, (p0T!p1), (Y(!p0Sp1)|!p0), Z(p0T!p1)]]\"\n"
+            + "[1] 2\n"
+            + "[!1] 1\n"
+            + "State: 2 \"[[p0, p1, Y(!p0Sp1), (Y(!p0Sp1)|!p0), (!p0Sp1)]," +
+            "[!p0, !p1, Y(!p0Sp1), (Y(!p0Sp1)|!p0), (!p0Sp1)]," +
+            "[!p1, p0, Y(!p0Sp1), (p0T!p1), (Y(!p0Sp1)|!p0)]," +
+            "[!p0, p1, Y(!p0Sp1), (Y(!p0Sp1)|!p0), (!p0Sp1)]]\"\n"
+            + "[0 & !1] 1\n"
+            + "[!0] 2\n"
+            + "[1] 2\n"
             + "--END--\n"
         )
       );
@@ -303,8 +340,9 @@ class Pltl2SafetyTest {
     @ParameterizedTest
     @MethodSource("provider")
     void test(String formula, int expectedSize, String expectedHoa) throws ParseException {
-      var labelledFormula = SpectraParser.parse(formula).getLabelledSafety().get(0);
-      var automaton = translator.apply(labelledFormula);
+      var labelledFormula = SpectraParser.parse(formula).getSafety().get(0);
+      var automaton = LTL2DAFunction.pSafety(environment, labelledFormula);
+
       assertEquals(expectedSize, automaton.size(), () -> HoaPrinter.toString(automaton));
       assertThat(automaton.acceptance(), AllAcceptance.class::isInstance);
 
@@ -316,6 +354,46 @@ class Pltl2SafetyTest {
         ),
         BuchiAcceptance.class
       );
+      assertTrue(LanguageAnalysis.contains(automaton1, automaton2));
+      assertTrue(LanguageAnalysis.contains(automaton2, automaton1));
+    }
+  }
+
+  @TestInstance(PER_CLASS)
+  @Nested
+  class PastEqFuture {
+    private List<Arguments> provider() {
+      return List.of(
+        Arguments.of(
+          "env boolean problem;\n"
+            + "sys boolean failure;\n"
+            + "asm G problem -> O failure;",
+          "!(!failure U problem)"
+        ),
+        Arguments.of(
+          "env boolean grant;\n"
+            + "sys boolean request;\n"
+            + "gar G (grant -> Y (!grant S request));",
+          "(request R !grant) & G (grant -> (request | (X (request R !grant))))"
+        )
+      );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void test(String spectraFormula, String ltlFormula) {
+      var pastLabelledFormula = SpectraParser.parse(spectraFormula).getSafety().get(0);
+      var pastAutomaton = LTL2DAFunction.pSafety(environment, pastLabelledFormula);
+
+      var futureLabelledFormula = LtlParser.parse(ltlFormula);
+      var futureAutomaton = LTL2DAFunction.safety(environment, futureLabelledFormula);
+
+      var automaton1 = Views.viewAs(pastAutomaton, BuchiAcceptance.class);
+      var automaton2 = Views.viewAs(futureAutomaton, BuchiAcceptance.class);
+
+      System.out.println(HoaPrinter.toString(pastAutomaton));
+      System.out.println(HoaPrinter.toString(futureAutomaton));
+
       assertTrue(LanguageAnalysis.contains(automaton1, automaton2));
       assertTrue(LanguageAnalysis.contains(automaton2, automaton1));
     }
