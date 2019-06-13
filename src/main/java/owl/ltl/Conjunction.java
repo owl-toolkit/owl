d789dcb2c1cc7468ddb3bbe7487cf1734488309b
Max Prokop <max.prokop@outlook.de>
2019-06-13 23:09:05 +0200

    all I did so far 13.06

--- src/main/java/owl/ltl/Conjunction.java
+++ src/main/java/owl/ltl/Conjunction.java
@@ -25,7 +25,6 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
-
 import owl.ltl.visitors.BinaryVisitor;
 import owl.ltl.visitors.IntVisitor;
 import owl.ltl.visitors.Visitor;
