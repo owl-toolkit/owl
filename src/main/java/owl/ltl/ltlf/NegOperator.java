398687aa04659fd76bbbeef6e5e25899d116f6b4
Max Prokop <max.prokop@outlook.de>
2019-06-14 12:21:49 +0200

    the other things I did so far 14. 06

---
+++ src/main/java/owl/ltl/ltlf/NegOperator.java
@@ -0,0 +1,65 @@
+package owl.ltl.ltlf;
+
+import owl.ltl.Formula;
+import owl.ltl.UnaryModalOperator;
+import owl.ltl.visitors.BinaryVisitor;
+import owl.ltl.visitors.IntVisitor;
+import owl.ltl.visitors.Visitor;
+
+import java.util.BitSet;
+import java.util.function.UnaryOperator;
+
+public class NegOperator extends UnaryModalOperator {// This Operator only exists so all LTLf Formulas can be represented, it does not contain any functionality
+  public NegOperator(Formula operand) {
+    super(NegOperator.class, operand);
+  }
+  @Override
+  public String operatorSymbol() {
+    return "!";
+  }
+
+  @Override
+  public int accept(IntVisitor visitor) {
+    return 0;
+  }
+
+  @Override
+  public <R> R accept(Visitor<R> visitor) {
+    return visitor.visit(this);
+  }
+
+  @Override
+  public <R, P> R accept(BinaryVisitor<P, R> visitor, P parameter) {
+    return null;
+  }
+
+  @Override
+  public boolean isPureEventual() {
+    return false;
+  }
+
+  @Override
+  public boolean isPureUniversal() {
+    return false;
+  }
+
+  @Override
+  public Formula nnf() {
+    return operand.not().nnf();
+  }
+
+  @Override
+  public Formula not() {
+    return operand;
+  }
+
+  @Override
+  public Formula unfold() {
+    return null;
+  }
+
+  @Override
+  public Formula unfoldTemporalStep(BitSet valuation) {
+    return null;
+  }
+}
