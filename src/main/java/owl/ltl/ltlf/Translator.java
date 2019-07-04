3e4fadf9b348a6e4ac10b9b8cce18ba76ebc1b43
Max Prokop <max.prokop@outlook.de>
2019-07-04 10:28:15 +0200

    added another last-Optimization case

--- src/main/java/owl/ltl/ltlf/Translator.java
+++ src/main/java/owl/ltl/ltlf/Translator.java
@@ -1,15 +1,16 @@
 package owl.ltl.ltlf;
 
-import java.util.List;
-
 import owl.ltl.Conjunction;
 import owl.ltl.Formula;
 import owl.ltl.GOperator;
 import owl.ltl.Literal;
 import owl.ltl.UOperator;
-import owl.ltl.visitors.PrintVisitor;
+
 
 public class Translator {
+  private Translator(){
+
+  }
   public static Formula translate(Formula in) {
     LtlfToLtlVisitor t = new LtlfToLtlVisitor();
     Literal Tail = Literal.of(in.atomicPropositions(true).length());
@@ -26,32 +27,7 @@ public class Translator {
 
   }
 
-  public int quotenformel() {
-    return 1;
-  }
 
-  public static String convToTlsf(List<String> inputs, List<String> outputs,
-                                   Literal Tail, Formula F, List<String> mapping) {
 
-    PrintVisitor P = new PrintVisitor(false,mapping);
-    StringBuilder out = new StringBuilder("INFO {\n"
-      + "  TITLE:       \"Test\"\n"
-      + "  DESCRIPTION: \"Test\"\n"
-      + "  SEMANTICS:   Mealy\n"
-      + "  TARGET:      Mealy\n"
-      + "}\n"
-      + "MAIN {\n"
-      + "  INPUTS {\n");
-    for (String l :inputs) {
-      out.append(l).append(";\n");
-    }
-    out.append("}\n" + "  OUTPUTS {\n");
-    for (String l :outputs) {
-      out.append(l).append(";\n");
-    }
-    out.append(Tail.toString()).append(";\n}\n").append(
-      "  GUARANTEE {\n").append(P.apply(F)).append(";\n}\n}");
 
-    return out.toString();
-  }
 }
