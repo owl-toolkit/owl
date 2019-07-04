f4c9c1d0c2a3adcad9a76322b40fc3ac978d7f18
Max Prokop <max.prokop@outlook.de>
2019-07-04 20:07:07 +0200

    added BiCondRewriter + Tests and Reimplemented the Translation so the main Formula is in coSafety. Also updated tests for Translator

---
+++ src/test/java/owl/ltl/visitors/ReplaceBiCondVisitorTest.java
@@ -0,0 +1,37 @@
+package owl.ltl.visitors;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import owl.ltl.Formula;
+import owl.ltl.ltlf.LtlfParser;
+import owl.ltl.rewriter.ReplaceBiCondVisitor;
+
+public class ReplaceBiCondVisitorTest {
+  private static final List<String> literals = List.of("a","b","c");
+  private static final List<Formula> Inputs = List.of(
+    LtlfParser.syntax("a <-> b",literals),
+    LtlfParser.syntax("G(a <-> b)",literals),
+    LtlfParser.syntax("G(a) <-> F(b)",literals),
+    LtlfParser.syntax("a <-> (b <-> c)",literals)
+  );
+  private static final List<Formula> Outputs = List.of(
+    LtlfParser.syntax("(!a | b) & (a|!b)",literals),
+    LtlfParser.syntax("G((!a | b) & (a|!b))",literals),
+    LtlfParser.syntax("(F !a | F b) & (G a | G !b)",literals),
+    LtlfParser.syntax("(!a | ((!b | c)&(b | !c)))&(a | ((!b | !c)&(b | c)))",literals)
+  );
+
+  @Test
+  void removeBicondTest() {
+
+    ReplaceBiCondVisitor r = new ReplaceBiCondVisitor();
+    for (int i = 0; i < Inputs.size(); i++) {
+      Formula f = r.apply(Inputs.get(i)).nnf();
+      Formula f1 = Outputs.get(i).nnf();
+      Assertions.assertEquals(f1,f);
+    }
+  }
+
+}
