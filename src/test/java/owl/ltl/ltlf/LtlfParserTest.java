0f6a9617ef269d96b9282056150c436d95919755
Max Prokop <max.prokop@outlook.de>
2019-07-04 10:26:15 +0200

    Fixed a Bug in the Parser for Conj and Disj, acctually added Tests

--- src/test/java/owl/ltl/ltlf/LtlfParserTest.java
+++ src/test/java/owl/ltl/ltlf/LtlfParserTest.java
@@ -1,4 +1,93 @@
 package owl.ltl.ltlf;
 
+
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.junit.jupiter.api.Test;
+
+import owl.ltl.BooleanConstant;
+import owl.ltl.Conjunction;
+import owl.ltl.Disjunction;
+import owl.ltl.FOperator;
+import owl.ltl.Formula;
+import owl.ltl.GOperator;
+import owl.ltl.LabelledFormula;
+import owl.ltl.Literal;
+import owl.ltl.MOperator;
+import owl.ltl.ROperator;
+import owl.ltl.UOperator;
+import owl.ltl.WOperator;
+import owl.ltl.XOperator;
+import owl.ltl.parser.LtlParser;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class LtlfParserTest {
+  private static final String[] INPUT = {
+    "!a",
+    "G a",
+    "F a & X b",
+    "(a -> b) U c",
+    "tt U b",
+    "a M b",
+    "a R b",
+    "!(a R b)",
+    "a W b U c R a",
+    "!(X !a)",
+    "tt & a",
+    "tt | a"
+  };
+  private static final Formula[] OUTPUT = {
+    new NegOperator(Literal.of(0)),
+    new GOperator(Literal.of(0)),
+    new Conjunction(new Formula[]{new FOperator(Literal.of(0)),new XOperator(Literal.of(1))}),
+    new UOperator(new Disjunction(new Formula[]{new NegOperator(Literal.of(0)),Literal.of(1)}),Literal.of(2)),
+    new UOperator(BooleanConstant.TRUE,Literal.of(0)),
+    new MOperator(Literal.of(0),Literal.of(1)),
+    new ROperator(Literal.of(0),Literal.of(1)),
+    new NegOperator(new ROperator(Literal.of(0),Literal.of(1))),
+    new WOperator(Literal.of(0),new UOperator(Literal.of(1),new ROperator(Literal.of(2),Literal.of(0)))),
+    new NegOperator(new XOperator(new NegOperator(Literal.of(0)))),
+    new Conjunction(new Formula[]{BooleanConstant.TRUE,Literal.of(0)}),
+    new Disjunction(new Formula[]{BooleanConstant.TRUE,Literal.of(0)})
+  };
+
+  @Test
+  void ParserTest(){
+    for (int i = 0;i< INPUT.length;i++){
+      assertEquals(OUTPUT[i],LtlfParser.syntax(INPUT[i]));
+    }
+  }
+
+  @Test
+  void testSingleQuotedLiteralParsing() {
+    LabelledFormula formula = LtlParser.parse("'a b c'");
+    assertEquals(List.of("a b c"), formula.variables());
+    assertEquals(Literal.of(0), formula.formula());
+  }
+
+  @Test
+  void testDoubleQuotedLiteralParsing() {
+    LabelledFormula formula = LtlParser.parse("\"a b c\"");
+    assertEquals(List.of("a b c"), formula.variables());
+    assertEquals(Literal.of(0), formula.formula());
+  }
+
+  @Test
+  void testParseRegression1() {
+    assertThrows(ParseCancellationException.class, () -> LtlParser.parse("FF"));
+  }
+
+  @Test
+  void testParseRegression2() {
+    assertThrows(ParseCancellationException.class, () -> LtlParser.parse("Fa!"));
+  }
+
+  @Test
+  void testParseRegression3() {
+    assertThrows(ParseCancellationException.class, () -> LtlParser.parse("F+"));
+  }
 }
--- src/test/java/owl/ltl/ltlf/TranslatorTest.java
+++ src/test/java/owl/ltl/ltlf/TranslatorTest.java
@@ -1,4 +1,81 @@
 package owl.ltl.ltlf;
 
+import org.junit.jupiter.api.Test;
+import owl.ltl.Formula;
+import owl.ltl.Literal;
+import owl.ltl.parser.LtlParser;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TranslatorTest {
+  private static final List<String> Literals = List.of("a", "b", "c", "d", "t");
+  private static final List<Formula> LtlfFORMULAS = List.of(
+    LtlfParser.syntax("false", Literals),
+    LtlfParser.syntax("true", Literals),
+    LtlfParser.syntax("a", Literals),
+
+    LtlfParser.syntax("! a", Literals),
+    LtlfParser.syntax("a & b", Literals),
+    LtlfParser.syntax("a | b", Literals),
+    LtlfParser.syntax("a -> b", Literals),
+    LtlfParser.syntax("a <-> b", Literals),
+    LtlfParser.syntax("a xor b", Literals),
+
+    LtlfParser.syntax("F a", Literals),
+    LtlfParser.syntax("G a", Literals),
+    LtlfParser.syntax("X a", Literals),
+
+    LtlfParser.syntax("a M b", Literals),
+    LtlfParser.syntax("a R b", Literals),
+    LtlfParser.syntax("a U b", Literals),
+    LtlfParser.syntax("a W b", Literals),
+
+    LtlfParser.syntax("(a <-> b) xor (c <-> d)", Literals),
+
+    LtlfParser.syntax("F ((a R b) & c)", Literals),
+    LtlfParser.syntax("F ((a W b) & c)", Literals),
+    LtlfParser.syntax("G ((a M b) | c)", Literals),
+    LtlfParser.syntax("G ((a U b) | c)", Literals),
+    LtlfParser.syntax("G (X (a <-> b))", Literals),
+    LtlfParser.syntax("G (X (a xor b))", Literals));
+
+  private static final List<Formula> LtlFORMULAS = List.of(
+    LtlParser.syntax("t & (t U (G !t)) & false", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & true", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & a", Literals),
+
+    LtlParser.syntax("t & (t U (G !t)) & (! a)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a & b)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a | b)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a -> b)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a <-> b)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a xor b)", Literals),
+
+    LtlParser.syntax("t & (t U (G !t)) & (F (t & a))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (G (!t | a))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (X (t & a))", Literals),
+
+    LtlParser.syntax("t & (t U (G !t)) & ((t & a) M b)", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a R (!t | b))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (a U (t & b))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & ((!t | a) W b)", Literals),
+
+    LtlParser.syntax("t & (t U (G !t)) & ((a <-> b) xor (c <-> d))", Literals),
+
+    LtlParser.syntax("t & (t U (G !t)) & (F (t & (a R (!t | b)) & c))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (F (t & ((!t | a) W b) & c))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (G (!t | (t & a) M b | c))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (G (!t |(a U (t & b)) | c))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (G (!t | X (t & (a <-> b))))", Literals),
+    LtlParser.syntax("t & (t U (G !t)) & (G (!t | X (t & (a xor b))))", Literals));
+
+  @Test
+  void testLtlfToLtlVisitor() {
+    for (int i = 0; i < LtlfFORMULAS.size(); i++) {
+      assertEquals(LtlFORMULAS.get(i),Translator.translate(LtlfFORMULAS.get(i), Literal.of(4)));
+    }
+  }
+
 }
