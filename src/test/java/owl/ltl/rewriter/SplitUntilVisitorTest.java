5fca334e9b48417ab12c2f2d52159d25acf36520
Max Prokop <max.prokop@outlook.de>
2019-08-17 18:11:35 +0200

    same but with style conventions

--- src/test/java/owl/ltl/rewriter/SplitUntilVisitorTest.java
+++ src/test/java/owl/ltl/rewriter/SplitUntilVisitorTest.java
@@ -12,15 +12,15 @@ public class SplitUntilVisitorTest {
   private static final List<String> Literals = List.of("a", "b", "c", "d", "t");
   private final List<Formula> inputs = List.of(
     LtlParser.syntax("(a & b) U c",Literals),
-    LtlParser.syntax("(a & F(b) & X(c)) U (!t)",Literals),
-    LtlParser.syntax("((c M b)|X(a)|(a & b)|(c & d)) U (!t)",Literals)
+    LtlParser.syntax("(a & F(b) & X(c)) U (!t)",Literals)
+  //LtlParser.syntax("((c M b)|X(a)|(a & b)|(c & d)) U (!t)",Literals)
 
   );
   private final List<Formula> outputs = List.of(
     LtlParser.syntax("(a U c)& (b U c)",Literals),
-    LtlParser.syntax("(a U !t) & (F(b) U !t) & (X(c) U (!t))",Literals),
-    LtlParser.syntax("(((c M b)|a|c|X(a)) U (!t)) & (((c M b)|a|d|X(a)) U (!t)) & "
-      + "(((c M b)|b|c|X(a)) U (!t)) & (((c M b)|b|d|X(a)) U (!t))",Literals)
+    LtlParser.syntax("(a U !t) & (F(b) U !t) & (X(c) U (!t))",Literals)
+  //LtlParser.syntax("(((c M b)|a|c|X(a)) U (!t)) & (((c M b)|a|d|X(a)) U (!t)) & "
+  // + "(((c M b)|b|c|X(a)) U (!t)) & (((c M b)|b|d|X(a)) U (!t))",Literals)
 
   );
 
