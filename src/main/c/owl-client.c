1e19923d0898e297b2c74b059ad7e097e75b2c52
Tobias Meggendorfer <tobias@meggendorfer.de>
2020-05-08 23:40:35 +0200

    Add missing changes

--- src/main/c/owl-client.c
+++ src/main/c/owl-client.c
@@ -22,10 +22,15 @@ int main(int argc, char *argv[])
     int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char buffer[BUFFER_SIZE];
 
-    if (argc != 4) {
-       fprintf(stderr, "usage %s hostname port formula\n", argv[0]);
-       exit(0);
+    if (argc == 2 && !strcmp(argv[1], "--version")) {
+        fprintf(stdout, "owl-client version 1.0");
+        exit(0);
+    }
+    if (argc < 3 || argc > 4) {
+       fprintf(stderr, "usage %s hostname port formula?\n", argv[0]);
+       exit(1);
     }
 
     // Open socket
@@ -51,27 +56,33 @@ int main(int argc, char *argv[])
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("Connect");
 
-    // Send request
-    if (send(sockfd, argv[3], strlen(argv[3]), 0) < 0 || send(sockfd, "\n", strlen("\n"), 0) < 0)
-        error("Write");
-    
+    if (argc == 4) {
+        // Send request
+        if (send(sockfd, argv[3], strlen(argv[3]), 0) < 0 || send(sockfd, "\n", strlen("\n"), 0) < 0)
+            error("Write");
+    } else {
+        while (fgets(buffer, BUFFER_SIZE, stdin)) {
+            if (send(sockfd, buffer, strlen(buffer), 0) < 0)
+                error("Write");
+        }
+    }
+
     // Close send channel
     if (shutdown(sockfd, SHUT_WR) < 0)
         error("Close Send");
-    
+
     // Recieve response
-    char buffer[BUFFER_SIZE];
     int recv_bytes = 0;
-    
+
     do {
         recv_bytes = recv(sockfd, buffer, sizeof(char) * BUFFER_SIZE, MSG_WAITALL);
-        
+
         if (recv_bytes < 0)
             error("Read");
 
         fwrite(buffer, sizeof(char), recv_bytes, stdout);
     } while (recv_bytes > 0);
-    
+
     // No need to shutdown the recieve channel, since the server shutdowns that.
     close(sockfd);
     return 0;
