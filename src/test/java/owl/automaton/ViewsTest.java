f716fdaef7b57c4d9be16622e144952bdeec9d78
Salomon Sickert <s.sickert@tum.de>
2020-06-23 16:28:49 +0200

    Update ViewsTest.java

--- src/test/java/owl/automaton/ViewsTest.java
+++ src/test/java/owl/automaton/ViewsTest.java
@@ -19,7 +19,8 @@
 
 package owl.automaton;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Map;
