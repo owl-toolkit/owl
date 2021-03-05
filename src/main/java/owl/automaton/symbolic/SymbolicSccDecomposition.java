4ff5e64f355eb136d59f28020f9c73225d868a40
Remco Abraham <remco1112@gmail.com>
2021-03-05 19:26:18 +0100

    Symbolic SCCs

---
+++ src/main/java/owl/automaton/symbolic/SymbolicSccDecomposition.java
@@ -0,0 +1,102 @@
+package owl.automaton.symbolic;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static owl.automaton.symbolic.SymbolicAutomaton.VariableType.COLOUR;
+import static owl.automaton.symbolic.SymbolicAutomaton.VariableType.STATE;
+
+import com.google.auto.value.AutoValue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import owl.bdd.BddSet;
+
+@AutoValue
+public abstract class SymbolicSccDecomposition {
+
+  public abstract SymbolicAutomaton<?> automaton();
+
+  public static SymbolicSccDecomposition of(SymbolicAutomaton<?> automaton) {
+    return new AutoValue_SymbolicSccDecomposition(automaton);
+  }
+
+  /**
+   * Computes the scc decomposition of the automaton using the algorithm
+   * described in https://doi.org/10.1007/s10703-006-4341-z, where the acceptance condition
+   * is included in the states. The returned BDDs therefore only contain state and colour variables
+   *
+   * @param restrictedTo a BddSet representing the state-space for which the SCC decomposition
+   *     is computed. Only states in restrictedTo are included in the result.
+   *
+   * @return A list of BddSets representing the SCCs of the automaton.
+   */
+  public List<BddSet> sccs(BddSet restrictedTo) {
+    if (restrictedTo.isEmpty()) {
+      return Collections.emptyList();
+    }
+    BddSet transitionRelation = automaton().transitionRelation();
+    SymbolicAutomaton.VariableAllocation variableAllocation = automaton().variableAllocation();
+    checkArgument(restrictedTo.factory() == transitionRelation.factory());
+    Deque<BddSet> worklist = new ArrayDeque<>();
+    worklist.push(restrictedTo);
+    List<BddSet> sccs = new ArrayList<>();
+    while (!worklist.isEmpty()) {
+      BddSet consideredStates = worklist.pop();
+      var states = variableAllocation.variables(STATE);
+      states.or(variableAllocation.variables(COLOUR));
+      // Get some arbitrary state from consideredStates
+      // (toSet returns a generator so there is no performance penalty here)
+      BddSet node = transitionRelation
+        .factory()
+        .of(consideredStates.toSet(states).iterator().next(), states);
+      BddSet forwardSet = node;
+      BddSet backwardSet = node;
+      BddSet successors = node;
+      BddSet predecessors = node;
+      // Compute successors and predecessors in lock-step
+      while (!successors.isEmpty() && !predecessors.isEmpty()) {
+        successors = automaton().successors(successors).intersection(consideredStates)
+          .intersection(forwardSet.complement());
+        predecessors = automaton().predecessors(predecessors).intersection(consideredStates)
+          .intersection(backwardSet.complement());
+        forwardSet = forwardSet.union(successors);
+        backwardSet = backwardSet.union(predecessors);
+      }
+      BddSet converged;
+      // Refine forwardset or backwardset
+      if (successors.isEmpty()) {
+        converged = forwardSet;
+        while (!predecessors.intersection(forwardSet).isEmpty()) {
+          predecessors = automaton().predecessors(predecessors).intersection(consideredStates)
+            .intersection(backwardSet.complement());
+          backwardSet = backwardSet.union(predecessors);
+        }
+      } else {
+        converged = backwardSet;
+        while (!successors.intersection(backwardSet).isEmpty()) {
+          successors = automaton().successors(successors).intersection(consideredStates)
+            .intersection(forwardSet.complement());
+          forwardSet = forwardSet.union(successors);
+        }
+      }
+      // Save the SCC
+      BddSet scc = forwardSet.intersection(backwardSet);
+      sccs.add(scc);
+      // Continue with the "recursive" steps of the algorithm
+      BddSet next1 = converged.intersection(scc.complement());
+      BddSet next2 = consideredStates.intersection(converged.complement());
+      if (!next1.isEmpty()) {
+        worklist.push(next1);
+      }
+      if (!next2.isEmpty()) {
+        worklist.push(next2);
+      }
+    }
+    return sccs;
+  }
+
+  public List<BddSet> sccs() {
+    return sccs(automaton().reachableStates());
+  }
+}
---
+++ src/test/java/owl/automaton/symbolic/SymbolicSccDecompositionTest.java
@@ -0,0 +1,198 @@
+package owl.automaton.symbolic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.BitSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import owl.automaton.Automaton;
+import owl.automaton.Views;
+import owl.automaton.acceptance.BuchiAcceptance;
+import owl.automaton.acceptance.RabinAcceptance;
+import owl.automaton.algorithm.SccDecomposition;
+import owl.ltl.LabelledFormula;
+import owl.ltl.parser.LtlParser;
+import owl.translations.LtlTranslationRepository;
+
+public class SymbolicSccDecompositionTest {
+
+  private static final Function<LabelledFormula, Automaton<?, ? extends RabinAcceptance>> LTL_TO_DRA
+    = LtlTranslationRepository.LtlToDraTranslation.EKS20.translation(RabinAcceptance.class);
+
+  private static final Function<LabelledFormula, Automaton<?, ? extends BuchiAcceptance>> LTL_TO_NBA
+    = LtlTranslationRepository.LtlToNbaTranslation.EKS20.translation(BuchiAcceptance.class);
+
+  private final List<LabelledFormula> tests = List.of(
+    "0",
+    "1",
+    "a",
+    "a & b",
+    "a | b",
+    "a -> b",
+    "a <-> b",
+    "a xor b",
+    "F a",
+    "G a",
+    "X a",
+    "a M b",
+    "a R b",
+    "a U b",
+    "a W b",
+    "F a & F b",
+    "F a & G b",
+    "F a & X b",
+    "F a | F b",
+    "F a | G b",
+    "F a | X b",
+    "G a & F b",
+    "G a & G b",
+    "G a & X b",
+    "G a | F b",
+    "G a | G b",
+    "G a | X b",
+    "X a & F b",
+    "X a & G b",
+    "X a & X b",
+    "X a | F b",
+    "X a | G b",
+    "X a | X b",
+    "X a | X !a",
+    "X X a | X X !a",
+    "X X X a | X X X !a",
+    "F a & X F !a",
+    "F a | X F !a",
+    "G a & X G !a",
+    "G a | X G !a",
+    "X F a",
+    "X X F a",
+    "X G a",
+    "X X G a",
+    "F G a",
+    "G F a",
+    "F G a & F G b",
+    "G F a & G F b",
+    "F G a & G F b",
+    "G F a & F G b",
+    "F G a | F G b",
+    "G F a | G F b",
+    "F G a | G F b",
+    "G F a | F G b",
+    "F G (a | X b)",
+    "F G (a & X b)",
+    "F G (a | X b | X X c)",
+    "F G (a & X b & X X c)",
+    "F (a & G b)",
+    "F (a | G b)",
+    "G (a & F b)",
+    "G (a | F b)",
+    "F (a M b)",
+    "F (a R b)",
+    "F (a U b)",
+    "F (a W b)",
+    "F X (a M b)",
+    "F X (a R b)",
+    "F X (a U b)",
+    "F X (a W b)",
+    "F ((X a) M b)",
+    "F ((X a) R b)",
+    "F ((X a) U b)",
+    "F ((X a) W b)",
+    "F (a M (X b))",
+    "F (a R (X b))",
+    "F (a U (X b))",
+    "F (a W (X b))",
+    "F ((a M b) & c)",
+    "F ((a R b) & c)",
+    "F ((a U b) & c)",
+    "F ((a W b) & c)",
+    "F ((a M b) | c)",
+    "F ((a R b) | c)",
+    "F ((a U b) | c)",
+    "F ((a W b) | c)",
+    "F ((a M b) & G c)",
+    "F ((a R b) & G c)",
+    "F ((a U b) & G c)",
+    "F ((a W b) & G c)",
+    "F ((a M b) | G c)",
+    "F ((a R b) | G c)",
+    "F ((a U b) | G c)",
+    "F ((a W b) | G c)",
+    "G (a M b)",
+    "G (a R b)",
+    "G (a U b)",
+    "G (a W b)",
+    "G X (a M b)",
+    "G X (a R b)",
+    "G X (a U b)",
+    "G X (a W b)",
+    "G ((X a) M b)",
+    "G ((X a) R b)",
+    "G ((X a) U b)",
+    "G ((X a) W b)",
+    "G (a M (X b))",
+    "G (a R (X b))",
+    "G (a U (X b))",
+    "G (a W (X b))",
+    "G ((a M b) & c)",
+    "G ((a R b) & c)",
+    "G ((a U b) & c)",
+    "G ((a W b) & c)",
+    "G ((a M b) | c)",
+    "G ((a R b) | c)",
+    "G ((a U b) | c)",
+    "G ((a W b) | c)",
+    "G ((a M b) & F c)",
+    "G ((a R b) & F c)",
+    "G ((a U b) & F c)",
+    "G ((a W b) & F c)",
+    "G ((a M b) | F c)",
+    "G ((a R b) | F c)",
+    "G ((a U b) | F c)",
+    "G ((a W b) | F c)",
+    "G ( a |  b) & GF ( b &  c)",
+    "G ( a | Xb) & GF ( b &  c)",
+    "G ( a |  b) & GF (Xb &  c)",
+    "G ( a | Xb) & GF (Xb &  c)",
+    "G ( a |  b) & GF ( b & Xc)",
+    "G ( a | Xb) & GF ( b & Xc)",
+    "G (Xa |  b) & GF (Xb &  c)",
+    "G (Xa | Xb) & GF (Xb &  c)"
+  ).stream().map(LtlParser::parse).collect(Collectors.toList());
+
+  @Test
+  void sccDecompositionTest() {
+    for (var formula : tests) {
+      var dra = Views.complete((Automaton) LTL_TO_DRA.apply(formula), new Object());
+      var nba = Views.complete((Automaton) LTL_TO_NBA.apply(formula), new Object());
+      assertSccs(Views.stateAcceptanceAutomaton(dra), SymbolicAutomaton.of(dra));
+      assertSccs(Views.stateAcceptanceAutomaton(nba), SymbolicAutomaton.of(nba));
+    }
+  }
+
+  private static void assertSccs(Automaton<?,?> expected, SymbolicAutomaton<?> actual) {
+    // Since there is no relation between the explicit and the symbolic states,
+    // we check whether the sizes of the SCCs are the same.
+    List<Integer> expectedSccs = SccDecomposition
+      .of(Views.stateAcceptanceAutomaton(expected))
+      .sccs()
+      .stream()
+      .map(Set::size)
+      .sorted()
+      .collect(Collectors.toList());
+    List<Integer> actualSccs = SymbolicSccDecomposition.of(actual).sccs()
+      .stream()
+      .map(bddSet -> {
+        var allocation = actual.variableAllocation();
+        BitSet variables = allocation.variables(SymbolicAutomaton.VariableType.STATE);
+        variables.or(allocation.variables(SymbolicAutomaton.VariableType.COLOUR));
+        return bddSet.toSet(variables);
+      })
+      .map(Set::size)
+      .sorted()
+      .collect(Collectors.toList());
+    assertEquals(expectedSccs, actualSccs);
+  }
+}
